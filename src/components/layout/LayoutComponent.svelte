ef02efe264e8e0ed8416d17d390e08ae4ed59e5f
ahsanarique <ahsan.arique@gmail.com>
2023-09-02 14:35:10 +0600

    mountain sticker

--- src/components/layout/LayoutComponent.svelte
+++ src/components/layout/LayoutComponent.svelte
@@ -1,3 +1,7 @@
+<script>
+  import MountainSticker from "./MountainSticker.svelte";
+</script>
+
 <section class="main-page layout-page">
   <div class="grid md:grid-cols-12 grid-rows-6 grid-cols-4 grid-flow-col">
     <div class="md:col-span-2 row-span-3 col-span-4 bg-blue-400"></div>
@@ -5,7 +9,9 @@
     <div class="md:col-span-2 row-span-2 col-span-4 bg-teal-400"></div>
     <div class="md:col-span-6 row-span-1 col-span-4 bg-fuchsia-400"></div>
     <div class="md:col-start-7 md:col-span-6 row-span-2 col-start-1 col-span-4 bg-cyan-400"></div>
-    <div class="md:col-start-7 md:col-span-4 md:row-span-4 md:row-start-3 row-start-4 row-span-3 col-start-1 col-span-4 bg-yellow-100"></div>
+    <div class="md:col-start-7 md:col-span-4 md:row-span-4 md:row-start-3 row-start-4 row-span-3 col-start-1 col-span-4 bg-yellow-100">
+      <MountainSticker />
+    </div>
     <div class="md:col-start-11 md:col-span-2 row-span-4 col-start-1 col-span-4 bg-emerald-400"></div>
   </div>
 </section>
---
+++ src/components/layout/MountainSticker.svelte
@@ -0,0 +1,71 @@
+<div class="mountain-sticker">
+  <div class="outer-square">
+    <div class="inner-circle">
+      <div class="sticker">
+        <div class="large-rect" />
+        <div class="small-rect" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .mountain-sticker {
+    display: flex;
+    height: 100%;
+  }
+
+  .outer-square {
+    --square-size: 300px;
+    --square-bg: #293462;
+
+    margin: auto;
+    display: flex;
+    height: var(--square-size);
+    width: var(--square-size);
+    background: var(--square-bg);
+  }
+
+  .outer-square .inner-circle {
+    --circle-size: 200px;
+
+    margin: auto;
+    display: flex;
+    height: var(--circle-size);
+    width: var(--circle-size);
+    border-radius: 50%;
+    overflow: hidden;
+    background: transparent;
+  }
+
+  .sticker {
+    --yellow-100: #fef9c3;
+    --rect-bg: #fe5f55;
+    --sticker-size: 200px;
+
+    rotate: 40deg;
+    margin: auto;
+    position: relative;
+    height: var(--sticker-size);
+    width: var(--sticker-size);
+    background: var(--yellow-100);
+  }
+
+  .sticker .large-rect {
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    height: 100%;
+    width: 100%;
+    background: var(--rect-bg);
+  }
+
+  .sticker .small-rect {
+    position: absolute;
+    top: 80%;
+    left: 35%;
+    height: 100%;
+    width: 100%;
+    background: var(--rect-bg);
+  }
+</style>
